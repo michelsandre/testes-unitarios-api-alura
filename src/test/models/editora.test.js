15deff5d926186f17a7b3cd1bb93e9e71fe6ac95
Andre Michels de Oliveira <michelsandre@gmail.com>
2024-02-02 12:50:21 -0300

    first issue

---
+++ src/test/models/editora.test.js
@@ -0,0 +1,42 @@
+import { expect } from '@jest/globals';
+import Editora from '../../models/editora.js';
+
+describe('Testando o modelo editora', () => {
+  const objetoEditora = {
+    nome: 'CDC',
+    cidade: 'Sao Paulo',
+    email: 'c@c.com',
+  };
+
+  it('Deve instanciar uma nova editora', () => {
+    const editora = new Editora(objetoEditora);
+
+    expect(editora).toEqual(expect.objectContaining(objetoEditora));
+  });
+
+  // metodo it.skip para pular o teste
+  it.skip('Deve salvar editora no DB', () => {
+    const editora = new Editora(objetoEditora);
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it('Deve salvar no DB usando sintaxe moderna', async () => {
+    const editora = new Editora(objetoEditora);
+
+    const dados = await editora.salvar();
+
+    const retornado = await Editora.pegarPeloId(dados.id);
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+        // eslint-disable-next-line comma-dangle
+      })
+    );
+  });
+});
